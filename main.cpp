a86d88da89643b44a31170c573c2bcb4ce96b965
COMPUTER102 <55975631+COMPUTER102@users.noreply.github.com>
2022-09-17 13:13:33 +0800

    Developing.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
-/*
-* Filename: main.cpp
-* Author: L.Y.
-* Brief: SHA256算法测试
-*/
+
 
 #include <iostream>
 #include <string>
+#include <exception>
+#include <stdexcept>
 
-#include "sha256.hpp"
+#include ".\hash_encrypt\sha256\sha256.hpp"
 
 using namespace std;
 /*using namespace std::cin;
@@ -16,25 +14,28 @@ using namespace std::string;*/
 
 string str;
 
-std::string char_cast_to_str(char text)
-{
+std::string char_cast_to_str(char text) {
 	//for (int i = 0, i <= t)
 }
 
-int main(int argc, char *argv[])
-{
-    cout << "Please enter the text to be encrypted: ";
-    cin >> str;
-	string message = str;                                    // 待加密的信息
-    ly::Sha256 sha256;                                           // 初始化Sha256
-	                                                             // 对象                                                             
-    string message_digest = sha256.getHexMessageDigest(message); // 加密
-    cout << message_digest << endl;                              // 输出加密结果
-    //cout <<"Press ENTER to continue..."<<endl;
-    getchar();
-    
-    //cout<<argv[0]<<endl;
-    
+int main(int argc, char *argv[]) {
+	try {
+		cout << "Please enter the text to be encrypted: ";
+		cin >> str;
+		string message = str;                                    // 待加密的信息
+		sha::sha256::Sha256 sha256;                                           // 初始化Sha256
+		// 对象
+		string message_digest = sha256.getHexMessageDigest(message); // 加密
+		cout << message_digest << endl;                              // 输出加密结果
+		cout <<"Press ENTER to continue..."<<endl;
+		getchar();
+
+		//throw runtime_error("test error");
+		//cout<<argv[0]<<endl;
+	} catch ( const Exception ) {
+		cerr << "[ERROR] Oh no! An error has occurred." << endl;
+	}
+
 	return 0;
 }
 
